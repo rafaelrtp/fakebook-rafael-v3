c8e05bddbeb322584f27db60cd589080754710fc
Julio Protzek <julioprotzek@gmail.com>
2012-10-26 21:57:53 -0200

    crud para status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fakebook::Application.config.secret_token = 'ff37a811edb5581e9848a3e779f97dbb6c2dca3f64d2b7c0f4f3ceec45dc7468ab33d18a9a11df0a2b9d17a14ebaeed6cd0c8b98629469b22a309686cd9c09fc'
